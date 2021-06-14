e5d926771701495758ae6c0e7050ab8f5268cf18
Ewa Forman <ewaforman@gmail.com>
2021-06-15 00:11:45 +0200

    mod

--- src/app/mycomp/mycomp.component.ts
+++ src/app/mycomp/mycomp.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import typy from '../typy.json';
 import { User1 } from './klasy';
 import { Game } from '../klasy2';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-mycomp',
@@ -11,57 +12,115 @@ import { Game } from '../klasy2';
 export class MycompComponent implements OnInit {
   // User: User1 = typy;
   game: Game = typy;
+  listaCegiel: Cegla[] = [];
 
   cegla: Cegla = {};
-  myarray: number[] =[];
+  myarray: number[] = [];
+  mecze: Mecz[] = [];
+
+  wynik:Map<string, Cegla[]> = new Map;
+
+
+  unikalnaListaSetow: Mecz[] = [];
 
   constructor() {}
 
+
+
   ngOnInit(): void {
-    // console.log(this.User)
-
-    // this.User.Turcja_Wlochy.
-
-    // console.log(Object.keys(this.game)[1])
-    // console.log(this.game['Nick'])
-    // this.game.key
     let set = new Set<string>();
+    let listaUnikalnychSetow = [];
+    let zmienna = new Map();
 
-    // Object.keys(this.game)['Turcja_Wlochy']
+    Object.keys(typy).forEach((klucz, index) => {
+      if(index > 0){
+ 
+        zmienna[klucz] = this.prepareMecz(klucz);
 
-    // console.log(this.game['Turcja_-_Wochy'])
-    this.game['Turcja_-_Wochy'].forEach((element) => {
-      set.add(element);
+
+      }
+
+
     });
-    // let dupa1: Array<string>;
+
+    this.wynik = zmienna;
+
+    console.log(zmienna)
+
     
-    let dupa = Array.from(set).sort();
 
-    // console.log(dupa)
+    // //////////////////
+    // this.game['Turcja_-_Wochy'].forEach((element) => {
+    //   set.add(element);
+    // });
+    // // let dupa1: Array<string>;
 
-    dupa.forEach((elementSet) => {
-      this.myarray = [];
-      // console.log("leci po secie")
+    // let dupa = Array.from(set).sort();
 
-      this.game['Turcja_-_Wochy'].forEach((element, index) => {
-        // console.log("leci po wynikach")
+    // // console.log(dupa)
 
-        if (elementSet == element) {
-          // console.log("pushh"+ elementSet + element)
-          this.myarray.push(index);
-        }
-      });
-      this.cegla[elementSet] = this.myarray;
-    });
+    // dupa.forEach((elementSet) => {
+    //   this.myarray = [];
+    //   // console.log("leci po secie")
 
-    console.log(this.cegla);
+    //   this.game['Turcja_-_Wochy'].forEach((element, index) => {
+    //     // console.log("leci po wynikach")
+
+    //     if (elementSet == element) {
+    //       // console.log("pushh"+ elementSet + element)
+    //       this.myarray.push(index);
+    //     }
+    //   });
+    //   this.cegla[elementSet] = this.myarray;
+    // }); ///////////////////////////////////
+
+    // console.log(this.cegla);
 
     //Add values
 
     // console.log(set)
   }
+
+  private prepareMecz(meczName: string) {
+    this.listaCegiel = []
+    let set = new Set<string>();
+    let array = [];
+    var cegielka = new Cegla();
+    let ostatniegowno= [];
+    this.game[meczName].forEach((element) => {
+      set.add(element);
+    });
+
+    let dupa = Array.from(set).sort();
+    
+
+    dupa.forEach((elementSet) => {
+      array = [];
+      var cegielka = new Cegla();
+
+      this.game[meczName].forEach((element, index) => {
+        if (elementSet == element) {
+          array.push(index);
+        }
+      });
+      // var cegielka = new Cegla();
+      this.cegla[elementSet] = array;
+      cegielka[elementSet] = array;
+
+
+      // console.log(cegielka)
+      this.listaCegiel.push(cegielka);
+      
+    });
+    return this.listaCegiel;
+  }
 }
 
-export interface Cegla {
+export class Cegla {
   [key: string]: number[];
+}
+
+export class Mecz {
+  name: string;
+  mapa = new Map(); // 1:0 [tomek, romek, atomek],
 }
